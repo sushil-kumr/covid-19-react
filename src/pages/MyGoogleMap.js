621df01c940b5d151c0537c1372c9c75e1f01a82
sushil-kumr <sushil.kumar@ves.ac.in>
2020-04-02 06:12:32 +0530

    Text Align in world

--- src/pages/MyGoogleMap.js
+++ src/pages/MyGoogleMap.js
@@ -8,7 +8,9 @@ import {
   InfoWindow
 } from "react-google-maps";
 
-import InfoWindowEx from "../component/InfoWindowEx";
+import Async from 'react-async';
+import Loader  from '../component/Loader'
+
 const demoFancyMapStyles = require("../js/mapStyles.json");
 
 const options = {
@@ -21,57 +23,53 @@ const Map = withScriptjs(withGoogleMap(props => {
 const [display, setDisplay] = useState('none');
 const [place, setPlace] = useState({});
 
-function onMarkerClick(data, b="block") {
+function onMarkerClick(data, b="inline-block") {
   setDisplay(b);
   setPlace(data);
+  // props.handleData(data,b);
 };
 
-  return (<>
-
-    <div style={{display: display, width: "100%"}}>
-              <br/>
-              <h6>{`Country/State: ${place.place}`}</h6>
-              <h6>Confirmed: {place.confirmed}</h6>
-              <h6>Deceased: {place.deaths}</h6>
-    </div>
-    <GoogleMap
-      defaultZoom={3}
-      defaultCenter={props.center}
-      defaultOptions={{ styles: demoFancyMapStyles, 
-                                mapTypeControl: false,
-                                zoomControl: true,
-                                streetViewControl: false,
-                                draggingCursor: 'move' 
-                      }}
-    >
-      {props.places.map(place => {
-        return (
-          <Fragment key={place.id}>
-            <Circle
-              onClick={()=>onMarkerClick(place)}
-              defaultCenter={{
-                lat: parseFloat(place.lat),
-                lng: parseFloat(place.lon)
-              }}
-              radius={place.confirmed*7}
-              options={options}
-            >
-          {/* <InfoWindow
-            visible={showingInfoWindow}
-          >
-            <div>
-              <h3>{selectedPlace.place}</h3>
-              <h6>Active Cases : {selectedPlace.active}</h6>
-              <h6>Confirmed Cases : {selectedPlace.confirmed}</h6>
-              <h6>Deaths Cases : {selectedPlace.deaths}</h6>
-              <h6>Recovered Cases : {selectedPlace.recovered}</h6>
-            </div>
-          </InfoWindow> */}
-          </Circle>
-          </Fragment>
-        );
-      })}
-    </GoogleMap></>
+  return (
+      <div className="row">
+                  <div  style={{display: display, width: "100%",textAlign:"center"}}>
+                    <br/>
+                    <h6>{`Country/State: ${place.place}`}</h6>
+                    <h6>Confirmed: {place.confirmed}</h6>
+                    <h6>Active: {place.confirmed-place.deaths-place.recovered}</h6>
+                    <h6>Deceased: {place.deaths}</h6>
+                    <h6>Recovered: {place.recovered}</h6>
+                  </div>
+                  <div className="col-lg-12">
+  
+                    <GoogleMap 
+                      defaultZoom={3}
+                      defaultCenter={props.center}
+                      defaultOptions={{ styles: demoFancyMapStyles, 
+                                                mapTypeControl: false,
+                                                zoomControl: true,
+                                                streetViewControl: false,
+                                                draggingCursor: 'move' 
+                                      }}
+                    >
+                      {props.places.map(place => {
+                        return (
+                          <Fragment key={place.id}>
+                            <Circle
+                              onClick={()=>onMarkerClick(place)}
+                              defaultCenter={{
+                                lat: parseFloat(place.lat),
+                                lng: parseFloat(place.lon)
+                              }}
+                              radius={place.confirmed*7}
+                              options={options}
+                            >
+                          </Circle>
+                          </Fragment>
+                        );
+                      })}
+                    </GoogleMap> 
+                  </div>
+      </div>
   );
 }))
 
@@ -80,45 +78,49 @@ export default class SampleMap extends Component {
   constructor(props){
     super(props)
     this.state ={
-      data:[],
+      values:{},
+      display:"none"
     }
   }
 
- 
-
-
-  componentDidMount() {
-    fetch('https://curecovid19.in/readings/readings/world_stats')
-    .then(res => res.json())
-    .then((data) => {
-      console.log(data);
-      this.setState({ data: data.data })
-    })
-    .catch(console.log)
-  }
+  // handleData = (d,b) => {
+  //   this.setState({values: d,
+  //                   display:b});
+  // }
 
   render() {
+    const loadUsers = () =>
+    fetch("https://curecovid19.in/readings/readings/world_stats")
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.json())
+    
     return (
-    <Map
-    center={{ lat: 41.8719, lng: 12.5674 }}
-    places={this.state.data}
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnt9meLwKfGzZtUZuyGB1iPp346rph9YA"
-    loadingElement={<div style={{ height: `100%` }} />}
-    containerElement={<div style={{ height: `700px` }} />}
-    mapElement={<div style={{ height: `85%` }} />}
-  />
-    )
-  }
+      <Async promiseFn={loadUsers} >
+      {({ data, err, isLoading }) => {
+          if (isLoading) return (<Loader/>)
+          if (err) return `Something went wrong: ${err.message}`
+          if (data) 
+          return(<>
+                  <h3 style={{textAlign:"center"}}>World Covid-19</h3>
+                  {/* <div style={{display:this.state.display, justifyContent:"center"}}>
+                  <h6>{`Country/State: ${this.state.values.place}`}</h6>
+                    <h6>Confirmed: {this.state.values.confirmed}</h6>
+                    <h6>Active: {this.state.values.confirmed-this.state.values.deaths-this.state.values.recovered}</h6>
+                    <h6>Deceased: {this.state.values.deaths}</h6>
+                    <h6>Recovered: {this.state.values.recovered}</h6>
+                  </div> */}
+                  <Map
+                    center={{ lat: 41.8719, lng: 12.5674 }}
+                    places={data.data}
+                    // handleData={this.handleData}
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnt9meLwKfGzZtUZuyGB1iPp346rph9YA"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `700px` }} />}
+                    mapElement={<div style={{ height: `85%` }} />}
+                  />    
+                </>
+          )
+  }}
+  </Async>
+    )}
 }
-
-
-// const SampleMap = () => {
-//   return (
-   
-//   )
-// }
-
-
-
-
-// export default (SampleMap);
