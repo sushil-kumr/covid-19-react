76193fad2e9ad394d7e13e8d24e9ca6b8584f1ab
sri-teja <asriteja77712@gmail.com>
2020-04-24 14:02:38 +0530

    Home update card

--- src/component/HomeUpdateCard.js
+++ src/component/HomeUpdateCard.js
@@ -14,20 +14,18 @@ export default class HomeUpdateCard extends Component {
             <div className="post-box mb-1">
             <div className="post-content pt-2 pb-0">
             <a href={this.props.data.link} target="" style={{textDecoration: "none"}}>
-                <p className="font-weight-bold" style={{fontSize:"16px", color: "black"}}>
+                <p className="font-weight-bold" style={{fontSize:"16px", color: "black", marginBottom: "4px"}}>
                     {this.props.data.headline}
                 </p>
                 <div className="post-foot">
-                <div className="post-tags col-10">
-                    <b style={{fontSize: "12px", color: "#555", marginRight: "1%"}}>{this.props.data.record_time}</b>
+                <div className="post-tags col-10" style={{paddingLeft: "0px"}}>
                     
                     <div className="badge badge-primary" style={{margin: "4px"}}>
                         {this.props.data.tags.split(',').slice(-1)["length"]? this.props.data.tags.split(',').slice(-1)[0] : "news"}
                     </div>
-                    
                 </div>
 
-                <div className="post-link"> <span> LINK</span><i className="fa fa-chevron-right"></i></div>
+                <div className="post-link"> <span>LINK</span><i className="fa fa-chevron-right"></i></div>
                 </div>
                 </a>
             </div>
